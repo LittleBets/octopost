837d7102722950f36c74cdb6d8d17304408b27d3
Ashok Gelal <ashokgelal@gmail.com>
2022-11-14 17:15:44 -0500

    fix: responsiveness

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -16,7 +16,7 @@ defineProps({
   <div
     class="items-top relative flex min-h-screen justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
   >
-    <div v-if="canLogin" class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
+    <div v-if="canLogin" class="fixed top-0 right-0 px-6 py-4">
       <Link
         v-if="$page.props.user"
         :href="route('compose.new')"
@@ -38,7 +38,7 @@ defineProps({
       </template>
     </div>
 
-    <div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
+    <div class="mx-auto my-auto max-w-6xl sm:px-6 lg:px-8">
       <div class="flex items-center justify-center gap-4 pt-8 sm:justify-start sm:pt-0">
         <ApplicationMark class="h-16 w-auto text-gray-700 sm:h-20" />
         <div class="text-5xl font-semibold uppercase text-gray-700">Octopost</div>
