2d2afe7350350106030b6097a5d3c33f519feadf
Ashok Gelal <ashokgelal@gmail.com>
2022-11-15 10:31:00 -0500

    tweak: colors

--- resources/js/Pages/Compose/EmptyResult.vue
+++ resources/js/Pages/Compose/EmptyResult.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mx-auto my-auto w-full max-w-2xl px-6">
-    <div class="relative block rounded-lg border-2 border-dotted border-gray-300 p-12 text-center">
-      <icon class="mx-auto h-10 w-10 text-gray-400" icon="ph:pencil-simple-line" />
-      <span class="mt-2 block text-lg font-medium text-gray-900"
+    <div class="relative block rounded-lg bg-gray-200 p-12 text-center shadow-inner">
+      <icon class="mx-auto h-8 w-auto text-gray-400" icon="ph:pencil-simple-line" />
+      <span class="mt-2 block font-medium text-gray-500"
         >Your composition results will appear here once available.</span
       >
     </div>
